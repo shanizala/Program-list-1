a7754cf6a2dd5ea8922b7c1bbda3fb783cf4c207
shanizala <132726720+shanizala@users.noreply.github.com>
2023-08-24 07:37:50 +0530

    l1p1a1.cpp

---
+++ l1p1a1.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+
+	cout<<"enrollment no 220130318007"<<endl;
+	cout<<"my name is sani zalavadiya"<<endl;
+
+	cout<<"Enter the number 1 ::";
+	cin>>a;
+
+	cout<<"Enter the number 2 ::";
+	cin>>b;
+
+	cout<<"sum of "<<a+b;
+
+	getch();
+}
