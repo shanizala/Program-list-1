2ddf6a58b22ab404130f9ede5c08365160ffd46c
shanizala <132726720+shanizala@users.noreply.github.com>
2023-08-24 07:48:10 +0530

    Create l1p8.cpp

---
+++ l1p8.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int p,r,t;
+	clrscr();
+
+	cout<<"220130318007"<<endl;
+	cout<<"sani zalavadiya"<<endl;
+
+	cout<<"Enter the amount::";
+	cin>>p;
+
+	cout<<"Enter the rate::";
+	cin>>r;
+
+	cout<<"Enter the time::";
+	cin>>t;
+
+	cout<<p*((1+r/100),t);
+
+	getch();
+}
