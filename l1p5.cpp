1940b16d04bda37ec7a635ae2b03daa4fffe5f2a
shanizala <132726720+shanizala@users.noreply.github.com>
2023-08-24 07:46:17 +0530

    l1p5.cpp

---
+++ l1p5.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a=5,b=6;
+	int temp;
+
+	cout<<"220130318007"<<endl;
+	cout<<"sani zalavadiya"<<endl;
+
+	cout<<"Enter the number::";
+
+	cout<<"a::"<<a<<endl<<"b::"<<b<<endl;
+
+
+	temp=a;
+	a=b;
+	b=temp;
+
+	cout<<"swap number is"<<endl;
+
+	cout<<"a::"<<a<<endl<<"b::"<<b<<endl;
+
+	getch();
+}
