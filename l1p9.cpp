b1621a6fa9910c85799049d9af8006200a7e8b84
shanizala <132726720+shanizala@users.noreply.github.com>
2023-08-24 07:54:13 +0530

    l1p9.cpp

---
+++ l1p9.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	clrscr();
+	cout<<"220130318007"<<endl;
+	cout<<"sani zalavadiya"<<endl;
+
+	cout<<"Enter the number::";
+	cin>>n;
+
+	if(n%2==0)
+	{
+		cout<<"This is Even Number";
+	}
+	else
+	{
+		cout<<"This is Odd Number";
+	}
+	getch();
+}
