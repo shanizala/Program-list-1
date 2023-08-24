dcda6325287e8366ed3b17d8179a0cc593805a15
shanizala <132726720+shanizala@users.noreply.github.com>
2023-08-24 07:39:21 +0530

    l1p1a2.cpp

---
+++ l1p1a2.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	cout<<"220130318007";
+	cout<<"sani zalavadiya";
+	cout<<"Enter the number:)";
+	cin>>a;
+	cout<<"Enter the number:)";
+	cin>>b;
+	cout<<a+b<<endl;
+	cout<<a-b<<endl;
+	cout<<a*b<<endl;
+	cout<<a/b<<endl;
+	cout<<a%b<<endl;
+	getch();
+}
